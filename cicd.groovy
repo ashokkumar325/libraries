486680d3a19beefa410fae3bf91ed64576503fb1
ashokkumar325 <108647657+ashokkumar325@users.noreply.github.com>
2022-07-16 22:57:22 +0530

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -7,3 +7,8 @@ def newmaven()
 {
    sh 'mvn package'
 }
+
+def newdeploy(jobname,ip,contestpath)
+{
+  sh label: '', script: 'scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/testwebapp.war'
+}
