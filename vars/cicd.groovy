6421e201db527b2c4b56022a3f7fe8c41fb76092
ashokkumar325 <108647657+ashokkumar325@users.noreply.github.com>
2022-07-16 23:42:14 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,14 @@
-def
+def newgit(repo)
+{
+  git "$(repo)"
+}
+
+def newmaven()
+{
+   sh 'mvn package'
+}
+
+def newdeploy(jobname,ip,contextpath)
+{
+  sh label: '', script: 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war'
+}
