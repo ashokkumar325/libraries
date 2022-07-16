56bd17b72d89b069ade97679f3caaaa898781e84
ashokkumar325 <108647657+ashokkumar325@users.noreply.github.com>
2022-07-17 00:20:44 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,3 +12,9 @@ def newdeploy(jobname,ip,contextpath)
 {
   sh label: '', script: "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contextpath}.war"
 }
+
+def runselenum(jobname)
+{
+     git 'https://github.com/intelliqittrainings/FunctionalTesting.git'
+  sh label: '', script: "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar" 
+}
